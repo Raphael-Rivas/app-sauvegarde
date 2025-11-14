3bc02e4b8f065adc7ae127e112f3946993b2a9b6
Raphael Rivas <raphael.rivas@etu.umontpellier.fr>
2025-11-14 18:06:59 +0100

    feat: init sendMsg client

--- src/client.py
+++ src/client.py
@@ -2,7 +2,19 @@ import socket
 from decouple import config
 
 HOST = config("HOST")
-PORT = config("PORT")
+PORT = int(config("PORT"))
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect((HOST, PORT))
+
+def sendMsg():
+    option = input("Choose between (save/restore/settings)")
+    while option != "save" or option != "restore" or option != "settings":
+        option = input("Choose between (save/restore/settings)")
+    match option:
+        case "save":
+            directory = input("Give the path of the directory you want to save")
+        case "restore":
+            path = input()
+        case "settings":
+            suffix = input()
