ba346ea749a36e589c6dd0b9359df5c32afbecbb
nicolasm <mael.nicolas@etu.umontpellier.fr>
2025-11-17 13:11:12 +0100

    feat: class SocketClient + recv_file + save

--- src/server.py
+++ src/server.py
@@ -5,10 +5,67 @@ from decouple import config
 HOST = config("HOST")
 PORT = int(config("PORT"))
 
+class SocketClient:
+
+    def __init__(self, socket):
+        self.socket = socket
+
+    def send(self, message):
+        self.socket.send(message.encode())
+
+    def recv(self):
+        return self.socket.recv(1024).decode()
+
+    def recv_file(self):
+        name = self.recv()
+        size = self.recv()
+        file = open(name, "wb")
+
+        file_bytes = b""
+        done = False
+        while not done:
+            data = self.recv()
+            if file_bytes[-5:] != b"<END>":
+                done = True
+            else:
+                file_bytes += data
+
+        file.write(file_bytes)
+        #file.close()
+        return name, file
+
+    def save(self):
+        data = self.recv()
+        while (data != "stop") :
+            name, file = self.recv_file()
+            file.close()
+
+
+    """
+    def restore(self):
+
+
+    def settings(self):
+
+    """
+
+    def close(self):
+        self.socket.close()
+
+
 def handle_client(s_client):
-    s_client.send("Hello")
-    data = s_client.recv(1024)
-    print(data.decode()[0])
+    s_client.send("Connexion réussie")
+    data = s_client.recv()
+    while data != "exit":
+        print("Message reçu : " + data)
+        match data:
+            case "save":
+                s_client.save()
+            case "restore":
+                s_client.restore()
+            case "settings":
+                s_client.settings()
+        data = s_client.recv()
     s_client.close()
 
 socket_ecoute = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,7 +79,8 @@ print("Début de l'écoute")
 while True:
     try:
         socket_client, adresse_client = socket_ecoute.accept()
-        client_thread = threading.Thread(target=handle_client, args=(socket_client,))
+        print("Connexion réussie")
+        client_thread = threading.Thread(target=handle_client, args=(SocketClient(socket_client),))
         client_thread.start()
 
         threads.append(client_thread)
